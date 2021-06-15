aa91c2e06c4a6478650aab490d4643088e92b43e
Hannenur Kıroğlu <hannenur.yazbahar@bil.omu.edu.tr>
2021-06-15 23:49:15 +0300

    Update cash.c

--- Week1/cash.c
+++ Week1/cash.c
@@ -25,19 +25,19 @@ float getValue(void){
 int coins(int cents){
     int count = 0;
     while(cents>=25){
-        cents = cents - 25;
+        cents -= 25;
         count++;
     }
     while(cents>=10){
-        cents = cents - 10;
+        cents -= 10;
         count++;
     }
     while(cents>=5){
-        cents = cents - 5;
+        cents -= 5;
         count++;
     }
     while(cents>=1){
-        cents = cents - 1;
+        cents -= 1;
         count++;
     }
     return count;
